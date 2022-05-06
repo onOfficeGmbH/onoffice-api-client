49ccaf34a59b172ba257265c941a01033f01b56f
Johannes Maas <j.maas@onoffice.de>
2022-05-06 17:36:34 +0200

    Refactor api-client

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,30 +1,40 @@
-import md5 from "md5"
+import md5 from "md5";
 
-export async function callApi(token: string, secret: string, call: string): Promise<object|null> {
-    const query = JSON.parse(call)
+export async function callApi(
+  token: string,
+  secret: string,
+  call: string,
+): Promise<object | null> {
+  const query = JSON.parse(call);
 
+  if (Array.isArray(query.parameters.data)) {
     // We need to sort the data for the HMAC.
-    query.parameters.data.sort()
-    const rawData = JSON.stringify(query.parameters)
+    query.parameters.data.sort();
+  }
 
-    // Escape special chars. Otherwise Umlaute, like in "Nürnberg", will not get recognized correctly in enterprise.
-    const toEscape = /[\u00C0-\u00FF]/g // Latin-1 Supplement, see https://en.wikipedia.org/wiki/List_of_Unicode_characters#Latin-1_Supplement
-    const escapedData = rawData.replace(toEscape, c => `\\u${('0000' + c.charCodeAt(0).toString(16)).slice(-4)}`)
+  const rawData = JSON.stringify(query.parameters);
 
-    const timestamp = Math.round(new Date().getTime() / 1000)
+  // Escape special chars. Otherwise Umlaute, like in "Nürnberg", will not get recognized correctly in enterprise.
+  const toEscape = /[\u00C0-\u00FF]/g; // Latin-1 Supplement, see https://en.wikipedia.org/wiki/List_of_Unicode_characters#Latin-1_Supplement
+  const escapedData = rawData.replace(
+    toEscape,
+    (c) => `\\u${("0000" + c.charCodeAt(0).toString(16)).slice(-4)}`,
+  );
 
-    const hmacparameters = `${escapedData},${token},${query.actionid},${query.identifier},${query.resourceid},${secret},${timestamp},${query.resourcetype}`
-    const hmac = md5(secret + md5(hmacparameters))
-      
-    const request = `{"token":"${token}","request":{"actions":[{"actionid":"${query.actionid}","resourceid":"${query.resourceid}","resourcetype":"${query.resourcetype}","identifier":"${query.identifier}","timestamp":${timestamp},"hmac":"${hmac}","parameters":${escapedData}}]}}`
-    
-    return fetch('http://localhost:8080', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: request
-    }).then(async (response) => 
-      response.json()
-    )
+  const timestamp = Math.round(new Date().getTime() / 1000);
+
+  const hmacparameters =
+    `${escapedData},${token},${query.actionid},${query.identifier},${query.resourceid},${secret},${timestamp},${query.resourcetype}`;
+  const hmac = md5(secret + md5(hmacparameters));
+
+  const request =
+    `{"token":"${token}","request":{"actions":[{"actionid":"${query.actionid}","resourceid":"${query.resourceid}","resourcetype":"${query.resourcetype}","identifier":"${query.identifier}","timestamp":${timestamp},"hmac":"${hmac}","parameters":${escapedData}}]}}`;
+
+  return fetch("http://localhost:8080", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: request,
+  }).then(async (response) => response.json());
 }
