385c2fa32fad97e66d93f9cffe2108bb4eb395bf
Johannes Maas <j.maas@onoffice.de>
2022-09-13 17:27:56 +0200

    Update to HMAC v2

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,6 +1,3 @@
-import md5 from "md5";
-import stableStringify from "json-stable-stringify";
-
 export async function callApi(
   token: string,
   secret: string,
@@ -8,24 +5,14 @@ export async function callApi(
 ): Promise<object | null> {
   const query = JSON.parse(call);
 
-  // We need to sort the data for the HMAC.
-  const rawData = stableStringify(query.parameters);
-
-  // Escape special chars. Otherwise Umlaute, like in "Nürnberg", will not get recognized correctly in enterprise.
-  const toEscape = /[\u00C0-\u00FF]/g; // Latin-1 Supplement, see https://en.wikipedia.org/wiki/List_of_Unicode_characters#Latin-1_Supplement
-  const escapedData = rawData.replace(
-    toEscape,
-    (c) => `\\u${("0000" + c.charCodeAt(0).toString(16)).slice(-4)}`,
-  );
-
   const timestamp = Math.round(new Date().getTime() / 1000);
+  const hmac = await generateHmac(token, secret, timestamp, query.resourcetype, query.actionid)
 
-  const hmacparameters =
-    `${escapedData},${token},${query.actionid},${query.identifier},${query.resourceid},${secret},${timestamp},${query.resourcetype}`;
-  const hmac = md5(secret + md5(hmacparameters));
+  const parameters = JSON.stringify(query.parameters);
 
   const request =
-    `{"token":"${token}","request":{"actions":[{"actionid":"${query.actionid}","resourceid":"${query.resourceid}","resourcetype":"${query.resourcetype}","identifier":"${query.identifier}","timestamp":${timestamp},"hmac":"${hmac}","parameters":${escapedData}}]}}`;
+    `{"token":"${token}","request":{"actions":[{"actionid":"${query.actionid}","resourceid":"${query.resourceid}","resourcetype":"${query.resourcetype}","identifier":"${query.identifier}","timestamp":${timestamp},"hmac_version":"2","hmac":"${hmac}","parameters":${parameters}}]}}`;
+
 
   return fetch(import.meta.env.VITE_API_SERVER_URL!! as string, {
     method: "POST",
@@ -34,4 +21,26 @@ export async function callApi(
     },
     body: request,
   }).then(async (response) => response.json());
+}
+
+async function generateHmac(token: string, secret: string, timestamp: number, resourceType: string, actionId: string): Promise<string> {
+  const hmacData = timestamp + token + resourceType + actionId
+
+  const encoder = new TextEncoder();
+  const key = await window.crypto.subtle.importKey(
+    "raw",
+    encoder.encode(secret),
+    {
+      name: "HMAC",
+      hash: "SHA-256",
+    },
+    false,
+    ["sign"],
+  );
+  const signed = await window.crypto.subtle.sign(
+    "HMAC",
+    key,
+    encoder.encode(hmacData),
+  );
+  return btoa(String.fromCharCode(...new Uint8Array(signed)));
 }
