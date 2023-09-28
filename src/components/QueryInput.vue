5ee301638a52ef11faf0719ddbfa9845caf5c60d
Dave <69651599+D4ve-R@users.noreply.github.com>
2023-09-28 18:38:10 +0200

    Update QueryInput

--- src/components/QueryInput.vue
+++ src/components/QueryInput.vue
@@ -1,275 +1,270 @@
 <script setup lang="ts">
-import { callApi } from '@/api-client';
-import { History } from '@/history';
-import { ref, reactive, computed, watch, onMounted } from 'vue';
+import { callApi, ApiResponse } from "@/api-client";
+import { History } from "@/history";
+import { ref, reactive, computed } from "vue";
 import { Codemirror } from "vue-codemirror";
 import { json } from "@codemirror/lang-json";
+import { getCredentials } from "@/session-storage";
+import TokenModal from "./TokenModal.vue";
 
-const savedToken = sessionStorage.getItem("token")
-const savedSecret = sessionStorage.getItem("secret")
-
-const token = ref(savedToken || "")
-const secret = ref(savedSecret || "")
-
-const rememberSession = import.meta.env.VITE_REMEMBER_SESSION as boolean | undefined || false
-function updateSessionStorage(token: string, secret: secret) {
-    if (rememberSession) {
-        sessionStorage.setItem("token", token)
-        sessionStorage.setItem("secret", secret)
-        localStorage.setItem("rememberSession", "true")
-    } else {
-        sessionStorage.removeItem("token")
-        sessionStorage.removeItem("secret")
-        localStorage.removeItem("rememberSession")
-    }
-}
-
-updateSessionStorage(token.value, secret.value)
-watch([token, secret], ([newToken, newSecret]) => {
-    updateSessionStorage(newToken, newSecret)
-})
+import createEstate from "@/assets/examples/create-estate.json";
+import readEstates from "@/assets/examples/read-estates.json";
+import readUser from "@/assets/examples/read-user.json";
+import readUserPhoto from "@/assets/examples/read-user-photo.json";
+import editEstate from "@/assets/examples/edit-estate.json";
+import readAddresses from "@/assets/examples/read-addresses.json";
+import createAddress from "@/assets/examples/create-address.json";
+import editAddress from "@/assets/examples/edit-address.json";
+import createSearchCriteria from "@/assets/examples/create-search-criteria.json";
 
 const extensions = [json()];
 
 const history = reactive(new History());
-const previousLabel = computed(() => {
-    if (history.canGoBack()) {
-        return `Previous (#${history.getCurrentNumber() - 1})`;
-    } else {
-        return "Previous";
-    }
-})
-const nextLabel = computed(() => {
-    if (history.canGoForward()) {
-        return `Next (#${history.getCurrentNumber() + 1})`;
-    } else {
-        return "Next"
-    }
-})
-function previousInHistoryClicked() {
-    history.goBack()
-    const historyEntry = history.getCurrentEntry()
-    if (historyEntry !== null) {
-        response.value = historyEntry.response
-    }
-}
-function nextInHistoryClicked() {
-    history.goForward()
-    const historyEntry = history.getCurrentEntry()
-    if (historyEntry !== null) {
-        response.value = historyEntry.response
-    }
-}
 
-const examples = reactive(new Map())
-examples.set("Read Estates", `\
-{
-    "actionid": "urn:onoffice-de-ns:smart:2.5:smartml:action:read",
-    "resourceid": "",
-    "identifier": "",
-    "resourcetype": "estate",
-    "parameters": {
-        "data":["Id", "lage", "kaufpreis"],
-        "filter": {
-            "status": [{"op": "=", "val": 1}],
-            "kaufpreis": [{"op": "<", "val": 300000}]
-        },
-        "listlimit": 100,
-        "listoffset": 0,
-        "sortby": {"kaufpreis": "ASC", "warmmiete": "ASC"}
-    }
-}`)
-examples.set("Create Estate", `\
-{ 
-    "actionid":"urn:onoffice-de-ns:smart:2.5:smartml:action:create",
-    "resourceid":"",
-    "identifier":"",
-    "resourcetype":"estate",
-    "parameters":{ 
-        "data":{ 
-            "objektart":"haus",
-            "nutzungsart":"wohnen",
-            "vermarktungsart":"kauf",
-            "objekttyp":"einfamilienhaus",
-            "plz":52068,
-            "ort":"Aachen",
-            "land":"DEU",
-            "regionaler_zusatz":["openGeoDb_Region_122117"],
-            "nebenkosten":100,
-            "heizkosten":80,
-            "mietpreis_pro_qm":12,
-            "wohnflaeche":75,
-            "anzahl_zimmer":3,
-            "anzahl_schlafzimmer":1,
-            "anzahl_badezimmer":1,
-            "breitengrad":"50.7762106",
-            "laengengrad":"6.0857545",
-            "heizungsart":["zentral","fussboden"],
-            "stellplatzart":["freiplatz","carport"],
-            "kaufpreis":200000
-        }
-    }
-}
-`)
+const examples = new Map<string, string>();
+examples.set("Read Estates", JSON.stringify(readEstates, undefined, 2));
+examples.set("Create Estate", JSON.stringify(createEstate, undefined, 2));
+examples.set("Edit Estate", JSON.stringify(editEstate, undefined, 2));
+examples.set("Read Addresses", JSON.stringify(readAddresses, undefined, 2));
+examples.set("Create Address", JSON.stringify(createAddress, undefined, 2));
+examples.set("Edit Address", JSON.stringify(editAddress, undefined, 2));
+examples.set(
+  "Create Search Criteria",
+  JSON.stringify(createSearchCriteria, undefined, 2)
+);
+examples.set("Read User", JSON.stringify(readUser, undefined, 2));
+examples.set("Read User Photo", JSON.stringify(readUserPhoto, undefined, 2));
+examples.set("Custom", JSON.stringify({}, undefined, 2));
 
-const currentExample = ref<string | null>("Read Estates")
-
-const query = ref<string>(examples.get(currentExample.value))
-const sending = ref(false)
-const error = ref<string | null>(null)
+const currentExample = ref("Read Estates");
+const query = ref(examples.get(currentExample.value) as string);
+const sending = ref(false);
+const error = ref<string | null>(null);
 const submitMsg = computed(() => {
-    if (!sending.value) {
-        return `Send #${history.size() + 1}`
-    } else {
-        return `Sending #${history.size() + 1}…`
-    }
-})
+  if (!sending.value) {
+    return `Send #${history.size() + 1}`;
+  } else {
+    return `Sending #${history.size() + 1}…`;
+  }
+});
 
-const response = ref("")
+const response = ref<ApiResponse | null>(null);
+const deltaTime = ref(0);
+const showTokenModal = ref(false);
 
 const codemirrorStyle = {
-    width: "100%",
-    "max-width": "100%",
-    "margin-top": "0.5rem",
-    "margin-bottom": "0.5rem",
-}
+  width: "100%",
+  "max-width": "100%",
+  "margin-top": "0.5rem",
+  "margin-bottom": "0.5rem",
+  border: "1px solid black",
+  "background-color": "white",
+};
 
 async function sendQuery() {
-    try {
-        error.value = null
-        sending.value = true
-        response.value = await callApi(
-            token.value,
-            secret.value,
-            query.value
-        ).then(response => JSON.stringify(response, undefined, 2))
-
-        history.addNewEntry({
-            query: query.value,
-            response: response.value
-        })
-    } catch (e: any) {
-        error.value = `An error occurred while sending the query:\n${e.message}`
-    } finally {
-        sending.value = false
+  try {
+    error.value = null;
+    sending.value = true;
+    const { savedToken, savedSecret } = getCredentials();
+    if (!savedToken || !savedSecret) {
+      showTokenModal.value = true;
+      return;
     }
+    const t = Date.now();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    response.value = await callApi(savedToken, savedSecret, query.value);
+    deltaTime.value = Math.round(Date.now() - t);
+
+    history.addNewEntry({
+      query: query.value,
+      response: JSON.stringify(response.value, undefined, 2),
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    error.value = `An error occurred while sending the query:\n${e.message}`;
+  } finally {
+    sending.value = false;
+  }
 }
 
 function queryModified(newQuery: string) {
-    query.value = newQuery
-    currentExample.value = null
+  query.value = newQuery;
 }
 
 function exampleChanged() {
-    if (currentExample.value !== null) {
-        query.value = examples.get(currentExample.value)
-    }
+  query.value = examples.get(currentExample.value) as string;
 }
 </script>
 
 <template>
-    <div class="input">
-        <h2>New query</h2>
-        <form @submit.prevent="sendQuery">
-            <div class="credentials">
-                <div class="inputs">
-                    <label>
-                        Token
-                        <input type="text" required v-model="token" name="username" autocomplete="username" />
-                    </label>
-                    <label>
-                        Secret
-                        <input type="password" required v-model="secret" name="password" autocomplete="password" />
-                    </label>
-                </div>
-            </div>
-            <select v-model="currentExample" @change="exampleChanged">
-                <option v-if="currentExample === null" selected :value="null">Custom</option>
-                <option v-for="[key, _] in examples" :value="key" :selected="currentExample === key">{{ key }}</option>
-            </select>
-            <codemirror class="cm-editor" :style="codemirrorStyle" :extensions="extensions" :modelValue="query"
-                @update:modelValue="queryModified" />
-            <input type="hidden" :value="query" name="query" />
-            <input type="submit" :value="submitMsg" :disabled="sending">
-        </form>
-        <div class="error" v-if="error !== null">
-            <p v-for="paragraph in error.split('\n')">{{paragraph}}</p>
+  <div class="container">
+    <div class="grid">
+      <form @submit.prevent="sendQuery">
+        <TokenModal @close="showTokenModal = false" :show="showTokenModal" />
+        <span>Pick an example or enter your own query.</span>
+        <select
+          v-model="currentExample"
+          @change="exampleChanged"
+          style="
+            width: fit-content;
+            border: 0.5px solid black;
+            border-radius: 0.5rem;
+          "
+        >
+          <option
+            v-for="[key, _] in examples"
+            :value="key"
+            :selected="currentExample === key"
+            :key="key"
+          >
+            {{ key }}
+          </option>
+        </select>
+        <codemirror
+          :style="codemirrorStyle"
+          :extensions="extensions"
+          :modelValue="query"
+          @update:modelValue="queryModified"
+        />
+        <input type="hidden" :value="query" name="query" />
+        <input
+          class="primary-button"
+          type="submit"
+          :value="submitMsg"
+          :disabled="sending"
+        />
+      </form>
+
+      <div class="grid-item">
+        <div style="width: 100%; display: flex; justify-content: space-between">
+          <h2>Response #{{ history.getCurrentNumber() }}</h2>
+          <div class="history">
+            <button :disabled="!history.canGoBack()" @click="history.goBack">
+              &laquo;
+            </button>
+            <span>{{ history.getCurrentNumber() }}</span>
+            <button
+              :disabled="!history.canGoForward()"
+              @click="history.goForward"
+            >
+              &raquo;
+            </button>
+          </div>
         </div>
-        <template v-if="history.size() > 0">
-            <h2>Response #{{history.getCurrentNumber()}}</h2>
-            <div class="history" v-if="history.size() > 1">
-                <button :disabled="!history.canGoBack()" @click="previousInHistoryClicked">{{ previousLabel }}</button>
-                <button :disabled="!history.canGoForward()" @click="nextInHistoryClicked">{{ nextLabel }}</button>
-            </div>
-            <details>
-                <summary>See query #{{history.getCurrentNumber()}}</summary>
-                <codemirror class="output" :style="codemirrorStyle" :extensions="extensions"
-                    :modelValue="history.getCurrentEntry()?.query" readonly />
-            </details>
-            <codemirror class="output" :style="codemirrorStyle" :extensions="extensions"
-                :modelValue="history.getCurrentEntry()?.response" readonly />
-        </template>
+        <div class="flex gap-4">
+          <span>Response time: {{ deltaTime }} ms</span>
+          <template v-if="response?.status">
+            <span class="flex items-center gap-1">
+              <pre
+                v-if="response?.status.code < 400"
+                class="h-2 w-2 bg-green-500 rounded-full"
+              ></pre>
+              <pre v-else class="h-2 w-2 bg-red-500 rounded-full"></pre>
+              Http: {{ response?.status.code }}
+            </span>
+            <span v-if="response?.status.errorcode > 0"
+              >ErrorCode: {{ response?.status.errorcode }}</span
+            >
+          </template>
+        </div>
+        <div class="error" v-if="error">
+          <p
+            v-for="paragraph in error?.split('\n')"
+            :key="paragraph.slice(0, 3)"
+          >
+            {{ paragraph }}
+          </p>
+        </div>
+        <details>
+          <summary>See query #{{ history.getCurrentNumber() }}</summary>
+          <codemirror
+            :style="codemirrorStyle"
+            :extensions="extensions"
+            :modelValue="history.getCurrentEntry()?.query"
+            disabled
+            readonly
+          />
+        </details>
+        <codemirror
+          v-if="!sending"
+          :style="codemirrorStyle"
+          :extensions="extensions"
+          :modelValue="history.getCurrentEntry()?.response"
+          disabled
+          readonly
+        />
+        <div v-else class="animate-pulse flex">
+          <div class="flex-1 py-1">
+            <div class="h-2 bg-[#036f99] rounded"></div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-.input {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+.grid {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
+@media screen and (min-width: 1000px) {
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 5%;
+  }
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 2rem;
+}
+
+.grid-item,
 form {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 100%;
-}
-
-.credentials {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 1.5rem;
-    gap: 0.5rem;
-}
-
-.credentials .inputs {
-    display: flex;
-    flex-direction: row;
-    gap: 1rem;
-}
-
-.credentials .remember {
-    flex-direction: row;
-}
-
-
-label {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 1rem;
+  width: 100%;
+  background-color: #fefefe;
+  border: 0.5px solid black;
+  border-radius: 0.5rem;
+  box-shadow: 0.05rem 0 0.25rem rgba(0, 0, 0, 0.25);
 }
 
 .error {
-    border: 0.1rem solid red;
-    padding: 0 1rem;
-    margin: 1rem 0;
+  border: 0.1rem solid red;
+  background-color: rgba(255, 10, 10, 0.25);
+  padding: 0 1rem;
+  margin: 1rem 0;
+  border-radius: 0.5rem;
 }
 
 .history {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    margin-bottom: 1rem;
-    gap: 0.5rem;
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
 }
 
 details {
-    width: 100%;
-    margin-bottom: 1rem;
+  width: 100%;
+  margin: 0.5rem 0;
 }
 
-.output {
-    width: 100%;
-    resize: none;
+button {
+  border: 0.5px solid black;
+  border-radius: 0.5rem;
+  padding: 0 0.5rem;
+  background-color: rgb(225, 225, 225);
+}
+
+button:disabled {
+  color: rgb(150, 150, 150);
+  background-color: rgb(200, 200, 200);
 }
 </style>
